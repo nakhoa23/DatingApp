e759575b745cde89bb4a0348c5b4590cdda0f5b2
ankhoa.dev@gmail.com <ankhoa.dev@gmail.com>
2024-07-21 23:50:46 +0700

    12-4

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,30 +1,57 @@
-import { Component, inject, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, OnInit, output } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   private accountService = inject(AccountService);
   private toastr = inject(ToastrService);
   cancelRegister = output<boolean>();
 
   model: any = {}
+  registerForm: FormGroup = new FormGroup({});
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('Hello', Validators.required),
+      password: new FormControl('', [Validators.required,
+        Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required,
+        this.matchValues('password')
+      ]),
+    })
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    })
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : {isMatching: true};
+    }
+  }
 
   register() {
-    this.accountService.register(this.model).subscribe({
-      next: respone => {
-        console.log(respone);
-        this.cancel();
-      },
-      error: error => this.toastr.error(error.error)
-    });
+    console.log(this.registerForm.value);
+    // this.accountService.register(this.model).subscribe({
+    //   next: respone => {
+    //     console.log(respone);
+    //     this.cancel();
+    //   },
+    //   error: error => this.toastr.error(error.error)
+    // });
   }
 
   cancel() {
